cfc56feaae39ebb87ce580eca1f12c8d01af9564
Ethan Bailey <ebailey256@gmail.com>
2025-01-13 12:25:40 -0500

    added freq file

--- create_device.lua
+++ create_device.lua
@@ -3,6 +3,7 @@ local log = hs.logger.new('createDev', 'debug')
 
 createDevice.hotkeys = {}
 createDevice.dataFile = '~/Music/Ableton/User Library/Remote Scripts/AAAremote/data/plugins.json'
+createDevice.freqFile = 'abcd_freq.json'
 
 -----------
 -- setup --
@@ -10,6 +11,7 @@ createDevice.dataFile = '~/Music/Ableton/User Library/Remote Scripts/AAAremote/d
 
 function createDevice:start()
     createDevice.deviceData = hs.json.read(createDevice.dataFile)
+    createDevice.freqData = hs.json.read(createDevice.freqFile)
     createDevice.chooser = hs.chooser.new(function(choice)
         return createDevice:select(choice)
     end)
@@ -36,10 +38,11 @@ end
 -- createDevice:show()
 -- Method
 -- Shows the device chooser
--- If the device chooser is already active (double press) then it calls createDevice:rebuild()
+-- If the device chooser is already active (double press) then it calls createDevice:refresh()
 function createDevice:show()
     if createDevice.chooser:isVisible() then
-        createDevice:rebuild()
+        createDevice:refresh()
+        hs.alert('refreshed device list')
     else
         createDevice.chooser:choices(createDevice.deviceData)
         createDevice.chooser:show()
@@ -58,6 +61,34 @@ function createDevice:select(choice)
 
     log.d(string.format('selected %s', choice['text']))
     createDevice.socket:send(string.format('create_plugin %s', choice['uri']), '0.0.0.0', 42069)
+
+    if createDevice.freqData[choice['text']] == nil then
+        createDevice.freqData[choice['text']] = 0
+    else
+        createDevice.freqData[choice['text']] = createDevice.freqData[choice['text']] + 1
+    end
+
+    hs.json.write(createDevice.freqData, createDevice.freqFile, true, true)
+    createDevice:refresh()
+end
+
+-- createDevice:refresh()
+-- Method
+-- Sorts the device table using the frequency data from createDevice.freqData
+-- Writes the list of devices to createDevice.dataFile
+function createDevice:refresh()
+    createDevice.deviceData = hs.json.read(createDevice.dataFile)
+    table.sort(createDevice.deviceData, function(left, right)
+        if createDevice.freqData[left['text']] == nil then
+            createDevice.freqData[left['text']] = 0
+        end
+        if createDevice.freqData[right['text']] == nil then
+            createDevice.freqData[right['text']] = 0
+        end
+        return createDevice.freqData[left['text']] > createDevice.freqData[right['text']]
+    end)
+    hs.json.write(createDevice.deviceData, createDevice.dataFile, true, true)
+    createDevice.chooser:choices(createDevice.deviceData)
 end
 
 return createDevice
